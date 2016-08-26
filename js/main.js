0b192801fc0cba5d8e08c24f134e1d75008f7f72
bilgin218 <bilgin218@gmail.com>
2016-08-26 23:51:27 +0300

    Fixes issue #3

--- js/main.js
+++ js/main.js
@@ -806,7 +806,7 @@ window.onload = function() {
 		//syncConfig(); 
 		//XHRTimer();		
 		}, screenOffTime);
-	
+	//Removing for S2 integration
 //	time2exit = setTimeout(function(){ 
 //		tizen.application.getCurrentApplication().exit();
 //		tizen.power.turnScreenOff();	
